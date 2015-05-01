3a2ad0e8d6a137403b843ef2636fca155f650938
unknown <jamesa@W7VDI-Jamesa.FMHI.com>
2015-05-01 15:07:04 -0500

    init commit

---
+++ src/J8ValidationReturn.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class J8ValidationReturn {
+	public boolean isValid = true;
+	public Map<String, Severity> errors = new HashMap<String, Severity>();
+	
+	public List<String> getErrorMessages() {
+		List<String> errorMsgs = new ArrayList<String>();
+		for(Map.Entry<String, Severity> entry : errors.entrySet()) {
+			errorMsgs.add(entry.getKey());
+		}
+		return errorMsgs;
+	}
+	
+	public List<String> getErrorMessagesBySeverity(Severity severity) {
+		List<String> errorMsgs = new ArrayList<String>();
+		for(Map.Entry<String, Severity> entry : errors.entrySet()) {
+			if(entry.getValue() == severity) {
+				errorMsgs.add(entry.getKey());
+			}
+		}
+		return errorMsgs;
+	}
+	
+	public boolean anyFatal() {
+		for(Map.Entry<String, Severity> entry : errors.entrySet()) {
+			if(entry.getValue() == Severity.Fatal) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean anyCritical() {
+		for(Map.Entry<String, Severity> entry : errors.entrySet()) {
+			if(entry.getValue() == Severity.Critical) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean anyWarning() {
+		for(Map.Entry<String, Severity> entry : errors.entrySet()) {
+			if(entry.getValue() == Severity.Warning) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public enum Severity {
+		Warning,
+		Critical,
+		Fatal
+	}
+}
